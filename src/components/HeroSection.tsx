7231e68a87c7881c21f304848561c8f620780523
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2025-02-19 20:01:12 +0000

    replace the text on the hero with my name, role, and a button that scrolls to the projects section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,20 +1,34 @@
 import { motion } from 'framer-motion';
 import { useAppContext } from '@/context';
 import { useTranslation } from 'react-i18next';
+import { Button } from '@/components/ui/Button';
 
 export default function HeroSection() {
   const { t } = useTranslation();
-  const { refs } = useAppContext();
+  const { refs, scrollToSection } = useAppContext();
 
   return (
     <motion.section
       id="home"
       ref={refs.home}
-      className="text-primary-foreground bg-primary p-4 min-h-[50vh] md:min-h-[80vh] flex flex-col justify-center"
+      className="text-primary-foreground bg-primary p-4 min-h-[50vh] md:min-h-[80vh] flex flex-col justify-center items-center gap-6"
     >
-      <h1 className="font-bold text-center text-3xl lg:text-4xl mx-2 lg:mx-20 select-none">
-        {t('sections.hero.description')}
-      </h1>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-primary-foreground font-extrabold text-5xl md:text-6xl lg:text-7xl select-none">
+          {t('sections.hero.name')}
+        </h1>
+        <h2 className="text-primary-foreground/75 font-semibold text-2xl md:text-3xl lg:text-4xl select-none">
+          {t('sections.hero.role')}
+        </h2>
+      </div>
+      <Button
+        size="lg"
+        variant="secondary"
+        className="focus-visible:ring-0 select-none"
+        onClick={() => scrollToSection('projects')}
+      >
+        {t('sections.hero.buttons.view-projects')}
+      </Button>
       <span className="sr-only">{t('sections.hero.sr-title')}</span>
     </motion.section>
   );
