e94af1435a38311691eda9ce4bf0288754fea0b2
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2025-06-28 11:13:06 +0100

    add spinner component

---
+++ src/components/ui/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="w-full flex justify-center items-center mt-10">
+      <div className="border-4 border-muted border-t-primary rounded-full animate-spin w-6 h-6" />
+    </div>
+  );
+}
